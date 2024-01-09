7065c9c5f785fd095c41bba9f4f8e18b33a59572
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-01-09 23:11:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import img from "./assets/background.png";
+
+const App = () => {
+  return (
+    <div className="bg-[url('./assets/background.png')] h-screen">App</div>
+  );
+};
+
+export default App;
