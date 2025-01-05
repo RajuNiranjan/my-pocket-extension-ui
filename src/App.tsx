7345dcc9670e26bd294a9b824084116f517fa6c2
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-01-05 23:21:50 +0530

    added user in store

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
 import { Navigate, Routes, Route } from "react-router-dom";
 import LogInScreen from "./pages/LogInScreen";
-import SignUpScreen from "./pages/SignUpScree";
 import HomeScreen from "./pages/HomeScreen";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchUser } from "./store/features/auth/authSlice";
+import { AppDispatch, RootState } from "./store/store";
+import SignUpScreen from "./pages/SignUpScree";
 
 const App = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  const { user, isLoading, error } = useSelector(
+    (state: RootState) => state.auth
+  );
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <p>Error: {error}</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="w-[450px] min-h-screen bg-white p-4 ">
+    <div className="w-[450px] min-h-screen bg-white p-4">
       <Routes>
-        <Route path="/login" element={<LogInScreen />} />
-        <Route path="/signup" element={<SignUpScreen />} />
-        <Route path="/" element={<HomeScreen />} />
+        {!user ? (
+          <>
+            <Route path="/login" element={<LogInScreen />} />
+            <Route path="/signup" element={<SignUpScreen />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        )}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </div>
--- src/store/features/auth/authSlice.ts
+++ src/store/features/auth/authSlice.ts
@@ -1,7 +1,14 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../../lib/axios";
+
+interface User {
+  _id: string;
+  userName: string;
+  email: string;
+}
 
 interface AuthState {
-  token: null | string;
+  user: User | null;
   isLoading: boolean;
   error: string | null;
 }
@@ -9,8 +16,29 @@ interface AuthState {
 const initialState: AuthState = {
   isLoading: false,
   error: null,
-  token: null,
+  user: null,
 };
+
+export const fetchUser = createAsyncThunk(
+  "auth/fetchUser",
+  async (_, { rejectWithValue }) => {
+    const token = localStorage.getItem("pocket");
+
+    if (!token) {
+      return rejectWithValue("No token found");
+    }
+    try {
+      const res = await axiosInstance.get("/auth/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error || "Failed to fetch user");
+    }
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
@@ -19,13 +47,31 @@ const authSlice = createSlice({
     authStart: (state) => {
       state.isLoading = true;
     },
-    authFailure: (state, action: PayloadAction<string>) => {
+    authFailure: (state, action: PayloadAction<string | null>) => {
       state.isLoading = false;
       state.error = action.payload;
     },
     authSuccess: (state) => {
       state.isLoading = false;
+      state.error = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchUser.fulfilled, (state, action: PayloadAction<User>) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.error = null;
+      })
+      .addCase(fetchUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error =
+          (action.payload as string) || "An error occurred while fetching user";
+      });
   },
 });
 
