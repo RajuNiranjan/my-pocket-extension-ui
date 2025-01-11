f1408f319c98afecb288463d78c58c875c5784f7
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-01-11 13:36:15 +0530

    setup with vite+ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,35 @@
-import { Navigate, Routes, Route } from "react-router-dom";
-import LogInScreen from "./pages/LogInScreen";
-import HomeScreen from "./pages/HomeScreen";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchUser } from "./store/features/auth/authSlice";
-import { AppDispatch, RootState } from "./store/store";
-import SignUpScreen from "./pages/SignUpScree";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const dispatch: AppDispatch = useDispatch();
-
-  const { user, isLoading, error } = useSelector(
-    (state: RootState) => state.auth
-  );
-
-  useEffect(() => {
-    dispatch(fetchUser());
-  }, [dispatch]);
-
-  if (isLoading) {
-    return (
-      <div className="w-full h-screen flex items-center justify-center">
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="w-full h-screen flex items-center justify-center">
-        <p>Error: {error}</p>
-      </div>
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="w-[450px] min-h-screen bg-white p-4">
-      <Routes>
-        {!user ? (
-          <>
-            <Route path="/login" element={<LogInScreen />} />
-            <Route path="/signup" element={<SignUpScreen />} />
-          </>
-        ) : (
-          <>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </>
-        )}
-        <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>
-    </div>
-  );
-};
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import App from "./App.tsx";
-import { store } from "./store/store.ts";
-
-import "./index.css";
-
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </StrictMode>
-  );
-} else {
-  console.error("Root element not found. Unable to render the app.");
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
