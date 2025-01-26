02201870cde6e982ca339c125038b575c2aa2880
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-01-26 23:22:28 +0530

    removed Chats related things

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,14 +2,12 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import AuthReducer from "./features/auth.slice";
 import PocketReducer from "./features/pocket.slice";
 import ThemeReducer from "./features/them.slice";
-import MessageReducer from "./features/message.slice";
 import MsgReducer from "./features/msg.slice";
 
 const rootReducers = combineReducers({
   auth: AuthReducer,
   pocket: PocketReducer,
   theme: ThemeReducer,
-  message: MessageReducer,
   msg: MsgReducer,
 });
 
